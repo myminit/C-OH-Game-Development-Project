62c95085f3b29d80cc8e1d2a2b7dbd72a0e53189
heiiodear <141292190+heiiodear@users.noreply.github.com>
2024-12-11 13:09:29 +0700

    Update EnemyMovement.cs

--- CohUnity/Scripts/Enemy/EnemyMovement.cs
+++ CohUnity/Scripts/Enemy/EnemyMovement.cs
@@ -1,42 +1,7 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//public class EnemyMovement : MonoBehaviour
-//{
-//    [SerializeField] private Transform[] patrolPoints;
-//    [SerializeField] private float moveSpeed;
-//    [SerializeField] private int patrolDestination;
-
-//    private void Update()
-//    {
-//        if (patrolDestination == 0)
-//        {
-//            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime);
-//            if (Vector2.Distance(transform.position, patrolPoints[0].position) < .2f)
-//            {
-//                transform.localScale = new Vector3(1, 1, 1);
-//                patrolDestination = 1;
-//            }
-//        }
-
-//        if (patrolDestination == 1)
-//        {
-//            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
-//            if (Vector2.Distance(transform.position, patrolPoints[1].position) < .2f)
-//            {
-//                transform.localScale = new Vector3(-1, 1, 1);
-//                patrolDestination = 0;
-//            }
-//        }
-//    }
-//}
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// คลาส abstract หลักสำหรับการเคลื่อนที่
 public abstract class EnemyMovement : MonoBehaviour
 {
     [SerializeField] protected Transform[] patrolPoints;
